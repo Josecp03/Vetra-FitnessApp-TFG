4a32d408f20c09e2068dee8834b6b00be11292da
José Corrochano Pardo <118207818+Josecp03@users.noreply.github.com>
2025-04-15 12:27:16 +0200

    Diálogo cerrar sesión diseñado

--- app/src/main/java/com/example/vetra_fitnessapp_tfg/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/vetra_fitnessapp_tfg/view/fragments/ProfileFragment.java
@@ -1,66 +1,64 @@
 package com.example.vetra_fitnessapp_tfg.view.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.view.ViewGroup.LayoutParams;
+import androidx.fragment.app.Fragment;
 import com.example.vetra_fitnessapp_tfg.R;
+import com.example.vetra_fitnessapp_tfg.databinding.FragmentProfileBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
+import java.util.Objects;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentProfileBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        // Listener para el botón de logut
+        binding.buttonLogOut.setOnClickListener(v -> showLogoutDialog());
+
+
+        return view;
+    }
+
+    private void showLogoutDialog() {
+
+        // Inflar la vista del diálogo
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_logout, null);
+
+        // Crear el BottomSheetDialog
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                requireContext(),
+                R.style.BottomSheetDialogTheme
+        );
+
+        // Asignar la vista inflada al diálogo
+        bottomSheetDialog.setContentView(dialogView);
+
+        // Ajustar fondo transparente
+        Objects.requireNonNull(bottomSheetDialog.getWindow())
+                .setBackgroundDrawableResource(android.R.color.transparent);
+
+        // Referenciar al botón Log out dentro del diálogo
+        MaterialButton buttonLogoutConfirm = dialogView.findViewById(R.id.buttonLogoutConfirm);
+        buttonLogoutConfirm.setOnClickListener(v -> {
+
+            // Lógica para cerrar sesión
+
+            // Cerrar el diálogo
+            bottomSheetDialog.dismiss();
+        });
+
+        // Mostrar el diálogo
+        bottomSheetDialog.show();
     }
 }
